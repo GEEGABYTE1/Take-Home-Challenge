fca5dec411c2034071561813deca40a5761fad21
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-10-23 14:39:31 -0400

    Word Filter

--- tree.py
+++ tree.py
@@ -2,23 +2,30 @@ from depth import dfs
 
 
 class TreeNode:
+    words = []
     def __init__(self, value):
         self.value = value 
         self.children = []
     
     def add_child(self, word, prev_node=None): 
-        count = 0
-        for letter in word:
-            letter = letter.lower()
-            
-            if self.letter_checker(letter, self.children) == False:
-                tree_object = TreeNode(letter)
-                self.add_letter(tree_object)
-                self = tree_object
-            else:
-                for treenode in self.children:
-                    if treenode.value == letter:
-                        self = treenode
+        word = word.lower()
+        word = word.strip(" ")
+        if word in self.words:
+            print("That word has been added")
+        else:
+            self.words.append(word)
+            count = 0
+            for letter in word:
+                letter = letter.lower()
+                
+                if self.letter_checker(letter, self.children) == False:
+                    tree_object = TreeNode(letter)
+                    self.add_letter(tree_object)
+                    self = tree_object
+                else:
+                    for treenode in self.children:
+                        if treenode.value == letter:
+                            self = treenode
                         
     
     def add_letter(self, child_node):
@@ -83,27 +90,32 @@ class TreeNode:
     def delete(self):
         user_prompt = str(input("Please type in a word you would like to delete: "))
         user_prompt = user_prompt.lower()
-        try:
-            user_prompt = user_prompt.lower()
-            user_letters = [i for i in user_prompt]
+        user_prompt.strip(" ")
+        if not user_prompt in self.words:
+            print("That word does not seem to be added to the trie")
+        else:
+            try:
+                user_prompt = user_prompt.lower()
+                user_letters = [i for i in user_prompt]
 
-            nodes = [self]
-            while len(nodes) > 0 and len(user_letters) > 0:
-                current_node = nodes.pop()
-                if current_node.value == user_letters[0]:
-                    current_node.value = None 
-                    user_letters.pop(0)
+                nodes = [self]
+                while len(nodes) > 0 and len(user_letters) > 0:
+                    current_node = nodes.pop()
+                    if current_node.value == user_letters[0]:
+                        current_node.value = None 
+                        user_letters.pop(0)
+                    
+                    nodes += current_node.children
                 
-                nodes += current_node.children
-            
-            if len(user_letters) == 0:
-                return True 
-        except:
-            return
+                if len(user_letters) == 0:
+                    return True 
+            except:
+                return
     
     def search_keyword(self):
         user_prompt = input("Please enter a word you would like to search: ")
         user_prompt = user_prompt.lower()
+        user_prompt = user_prompt.strip(" ")
         user_letters = [i for i in user_prompt]
 
         returned_path = []
